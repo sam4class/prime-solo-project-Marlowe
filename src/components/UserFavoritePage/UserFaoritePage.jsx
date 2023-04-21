8ee669d729a0543b74aa44138f3ba459b1ab614d
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-04-21 10:48:59 -0500

    added text color to lakes relating to water_quality_status

--- src/components/UserFavoritePage/UserFaoritePage.jsx
+++ src/components/UserFavoritePage/UserFaoritePage.jsx
@@ -29,30 +29,50 @@ function UserFavoritePage(){
         <h1>Your Lakes</h1>
        {favLakeList.map((item) => {
             return(<>
-                <ul key={item.id}>
-                    
-                      <li onClick={() => {
+                  <ul key={item.id}>
+                        {item.water_quality_status === 'GOOD' ? 
+                        <li className='green' onClick={() => {
+                            console.log('clicking')
                             dispatch({
                                 type: 'WATER_DATA',
                                 payload: item.id
                             });
                             history.push('/waterData')
-                        }}>{item.name}</li>
-                </ul>
+                        }}>{item.name}</li>:
+                        item.water_quality_status === 'FAIR' ?
+                        <li className='yellow' onClick={() => {
+                            console.log('clicking')
+                            dispatch({
+                                type: 'WATER_DATA',
+                                payload: item.id
+                            });
+                            history.push('/waterData')
+                        }}>{item.name}</li>:
+                        item.water_quality_status === 'BAD' ?
+                        <li className='red' onClick={() => {
+                            console.log('clicking')
+                            dispatch({
+                                type: 'WATER_DATA',
+                                payload: item.id
+                            });
+                            history.push('/waterData')
+                        }}>{item.name}</li>: <></>
+                        }
+                        </ul>
             </>)
         })}
      <button onClick={toNotesPage}>Make a Note</button>
     <hr/>
     <div>
         {/* <NotePage /> */}
-       
+       <h1>Lake Notes</h1>
        {userNotes ?
           userNotes.map((item) => {
             return(<>
             <ul key={item.id}>
             <p>Lakes: {item.name}</p>
-            <li>Date: {item.date}</li>
-            <li>Note: {item.note}</li> 
+            <p>Date: {item.date}</p>
+            <p>Note: {item.note}</p> 
             <button id={item.id} onClick={() => {
               dispatch({
                 type: 'DELETE_NOTE',
