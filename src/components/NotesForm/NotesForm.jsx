79966b422403201d1ac5707a8c75499e065f3009
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-04-22 20:22:44 -0500

    working on dropdown label currently broken

--- src/components/NotesForm/NotesForm.jsx
+++ src/components/NotesForm/NotesForm.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
 
 
-function NotesForm(){
+function NotesForm() {
     console.log('inside NotesForm() page')
     const history = useHistory();
     const dispatch = useDispatch();
@@ -14,7 +16,7 @@ function NotesForm(){
     const lakeList = useSelector((store) => store.lakeList);
     // console.log('the lakeList', lakeList);
 
-    function addNewNote(event){
+    function addNewNote(event) {
         // console.log('inside addNewNote()')
         event.preventDefault();
 
@@ -34,28 +36,79 @@ function NotesForm(){
         history.push('/user');
     }
 
-    function cancelNote(){
+    function cancelNote() {
         history.push('/user');
     }
 
-    return(<>
-        <h1>Add A Note:</h1>
-
-        <form onSubmit={addNewNote}>
-            <select value={addLake} onChange={(event) => setAddLake(event.target.value)}>
-                <option label='Which Lake Are You At?' value="empty"></option>
-                {lakeList.map((item) => {
-                    return(
-                    <option key={item.id} label={item.name} value={item.id}></option>
+    return (<>
+        <Typography
+            m={3}
+            variant="h5"
+            color="primary">
+            Add A Note:
+        </Typography>
+        <form
+            noValidate
+            onSubmit={addNewNote}>
+            <FormControl variant="outlined" style={{ marginTop: 20, marginLeft: 0 }}>
+            <InputLabel shrink>Lake Name</InputLabel>
+            <Select label="Lake Name"value={addLake} onChange={(event) => setAddLake(event.target.value)}>
+                {/* <option label='Which Lake Are You At?' value="empty"></option> */}
+                <MenuItem value={lakeList.map((item) => {
+                    return (
+                        <option key={item.id} label={item.name} value={item.id}></option>
                     )
-                })}
-            </select>
-            <input type='text' placeholder='Date 01-01-2023' value={addDate} onChange={(event) => setAddDate(event.target.value)}/>
-            <input type='text' placeholder='Note' value={addNote} onChange={(event) => setAddNote(event.target.value)}/>
-            <button type='submit'>Add Note</button>
-            <button onClick={cancelNote}>Cancel</button>
+                })}></MenuItem>
+            </Select>
+            </FormControl>
+            <br/>
+            <TextField
+                label="Date"
+                placeholder="01-01-2023"
+                variant="outlined"
+                color="primary"
+                margin="normal"
+                value={addDate}
+                onChange={(event) => setAddDate(event.target.value)}
+            />
+            {/* <input type='text' placeholder='Date 01-01-2023' value={addDate} onChange={(event) => setAddDate(event.target.value)}/> */}
+            <TextField
+                label="Note"
+                variant="outlined"
+                color="primary"
+                multiline
+                rows={4}
+                margin="normal"
+                fullWidth
+                value={addNote}
+                onChange={(event) => setAddNote(event.target.value)}
+            />
+            <br />
+            {/* <input type='text' placeholder='Note' value={addNote} onChange={(event) => setAddNote(event.target.value)}/> */}
+            <Box
+                m={1}
+                display="flex"
+                justifyContent="flex-end"
+                alignItems="flex-end">
+                <Button
+                    variant="contained"
+                    size="small"
+                    type='submit'>Add Note</Button>
+            </Box>
+
+            <Box
+                m={1}
+                display="flex"
+                justifyContent="flex-end"
+                alignItems="flex-end">
+                <Button
+                    variant="outlined"
+                    size="small"
+                    className="formBtn"
+                    onClick={cancelNote}>Cancel</Button>
+            </Box>
         </form>
-        </>)
+    </>)
 }
 
 export default NotesForm;
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useDispatch, useSelector } from 'react-redux';
-import NotesForm from '../NotesForm/NotesForm';
 import { useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
-import UserFavoritePage from '../UserFavoritePage/UserFaoritePage';
+import { Box, Button, Typography } from '@mui/material';
 
 function UserPage() {
   // console.log('in UserPage()')
@@ -13,12 +12,8 @@ function UserPage() {
   const history = useHistory();
   const user = useSelector((store) => store.user);
   const lakeList = useSelector((store) => store.lakeList);
-  const  userNotes = useSelector((store) => store.userNotes);
+  // const  userNotes = useSelector((store) => store.userNotes);
 
-
-  // function toNotesPage(){
-  //   history.push('/notes')
-  // }
 
   function handleOnboarded(){
     dispatch({
@@ -39,25 +34,46 @@ function UserPage() {
   
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
+                  <Typography variant="h4"
+                mt={4}
+                mb={2}
+                color="primary"
+                align="center"
+            className="header"
+            >
+              Welcome, {user.username}!
+            </Typography>
+     
       {/* <p>Your ID is: {user.id}</p> */}
-      <h3>Pick Three Lakes to Watch:</h3>
+      <center>
+      <Typography variant="h4"
+                mt={4}
+                mb={2}
+                color="primary"
+                align="center"
+            className="header"
+            >
+               <h3>Pick Three Lakes to Watch:</h3>
+            </Typography>
+            </center>
       
       {lakeList.length ?
       <>
             {lakeList.map((item) => {
                 return (
                     <ul key={item.id}> 
-                        <li>{item.name}</li>
+                        <li className="listBox">{item.name}</li>
                         
-                        <button onClick={() => {
-                          console.log('clicking for fav!', item.id)
+                        <Button 
+                        variant='primary'
+                        onClick={() => {
+                          // console.log('clicking for fav!', item.id)
                           dispatch({
                             type: 'FAV_LAKES', 
                             payload: {
                               lakes_id: item.id,
                             }});
-                        }}>Fav Lake</button>
+                        }}>Fav Lake</Button>
                     </ul>)
                     
             })} 
